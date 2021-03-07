996f7a279a20a5db1144cf8c92a49b39d8b0cb46
technodromex <ricardo.guzmantorres@owasp.org>
2021-03-06 19:02:47 -0600

    Update switch theme option

--- src/app/main-toolbar/main-toolbar.component.ts
+++ src/app/main-toolbar/main-toolbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
+import { 
+  Component, OnInit, OnDestroy, 
+  Input, Output, EventEmitter
+} from '@angular/core';
 import {MatBottomSheet, MatBottomSheetRef} from '@angular/material/bottom-sheet';
 import { ModalLoginOptionsComponent } from '../shared/modal-login-options/modal-login-options.component';
 import { AuthService } from '../auth/auth.service';
@@ -6,6 +9,7 @@ import { ArweaveService } from '../auth/arweave.service';
 import { Subscription, EMPTY, Observable } from 'rxjs';
 import { INetworkResponse } from '../auth/INetworkResponse';
 import {MatSnackBar} from '@angular/material/snack-bar';
+declare const document: any;
 
 @Component({
   selector: 'app-main-toolbar',
@@ -53,6 +57,28 @@ export class MainToolbarComponent implements OnInit, OnDestroy {
       verticalPosition: verticalPosition,
       panelClass: panelClass
     });
+  }
+
+  setMainTheme(theme: string) {
+    const _ts = document.getElementById('LINK_MAIN_TEMPLATE');
+    if (!_ts) {
+      this.message('Error updating theme', 'error')
+      return;
+    }
+
+    switch (theme) {
+      case 'indigo-pink':
+        _ts.href = `./assets/css/${theme}.css`;
+      break;
+      case 'pink-bluegrey':
+        _ts.href = `./assets/css/${theme}.css`;
+      break;
+      default:
+      break;
+    }
+    
+
+
   }
 
 }
