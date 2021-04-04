e0d7ca1e2f2c4be3a3c9d8fb029b476d836bc78f
technodromex <ricardo.guzmantorres@owasp.org>
2021-04-03 22:14:35 -0600

    Update dashboard

--- src/app/contracts/wisdom-wizards-token.ts
+++ src/app/contracts/wisdom-wizards-token.ts
@@ -4,8 +4,6 @@ import {
 } from 'smartweave';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ArweaveService } from '../auth/arweave.service';
-import { AuthService } from '../auth/auth.service';
 import Community from 'community-js';
 
 
@@ -16,16 +14,20 @@ export class WisdomWizardsTokenContract
 {
 	private _contractAddress: string = 'eCUK6Jrt30GKy_EKkbgExt1G0Qf_AhNAvzu4977E5sw';
 	private _community: Community|null = null;
-	constructor(private _arweave: ArweaveService, private _auth: AuthService) {
-		this._community = new Community(this._arweave.arweave, this._auth.getPrivateKey());
+	constructor() {
+		
+	}
+
+	initCommunity(_arweave: any, privateKey: any) {
+		this._community = new Community(_arweave, privateKey);
 		this._community.setCommunityTx(this._contractAddress);
 	}
 	/*
 	*	@dev Get full contract state as Observable
 	*/
-	getState(): Observable<any> {
+	getState(_arweave: any): Observable<any> {
 		const obs = new Observable((subscriber) => {
-			readContract(this._arweave.arweave, this._contractAddress).then((state) => {
+			readContract(_arweave, this._contractAddress).then((state) => {
 				subscriber.next(state);
 				subscriber.complete();
 			}).catch((error) => {
@@ -45,6 +47,23 @@ export class WisdomWizardsTokenContract
 		const obs = new Observable((subscriber) => {
 			this._community!.getState().then((state) => {
 				subscriber.next(state);
+				subscriber.complete();
+			}).catch((error) => {
+				subscriber.error(error);
+			});
+
+		});
+
+		return obs;
+	}
+
+	/*
+	 *	@dev Get account balance in WWT
+	 */
+	getPSTBalance(account: string) {
+		const obs = new Observable((subscriber) => {
+			this._community!.getBalance().then((balance) => {
+				subscriber.next(balance);
 				subscriber.complete();
 			}).catch((error) => {
 				subscriber.error(error);
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,11 +4,13 @@ import {MatSnackBar} from '@angular/material/snack-bar';
 import { ArweaveService } from '../auth/arweave.service';
 import { Observable, Subscription, EMPTY } from 'rxjs';
 import { WisdomWizardsContract } from '../contracts/wisdom-wizards';
+import { WisdomWizardsTokenContract } from '../contracts/wisdom-wizards-token';
 import { MatDialog } from '@angular/material/dialog';
 import {
   ModalRegisterComponent
 } from '../shared/modal-register/modal-register.component';
 import { AuthService } from '../auth/auth.service';
+import { selectWeightedPstHolder } from 'smartweave';
 declare const window: any;
 
 @Component({
@@ -19,6 +21,7 @@ declare const window: any;
 export class DashboardComponent implements OnInit, OnDestroy {
 	mainAddress: string = this._auth.getMainAddressSnapshot();
 	balance: Observable<string> = this._arweave.getAccountBalance(this.mainAddress);
+  balancePST: string = '';
 	state: any = null;
 	state$: Subscription = Subscription.EMPTY;
 	register$: Subscription = Subscription.EMPTY;
@@ -29,6 +32,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
   txmessage: string = '';
   lastTransactionID: Observable<string> = this._arweave.getLastTransactionID(this.mainAddress);
   otherDataInterval: any = null;
+  probReward: string = '';
 
   constructor(
   	private _router: Router,
@@ -36,22 +40,33 @@ export class DashboardComponent implements OnInit, OnDestroy {
   	private _arweave: ArweaveService,
   	private _wisdomWizards: WisdomWizardsContract,
     private _dialog: MatDialog,
-    private _auth: AuthService
+    private _auth: AuthService,
+    private _wisdomWizardsToken: WisdomWizardsTokenContract
   ) { }
 
   ngOnInit(): void {
   	this.loading = true;
-
+  
   	// Fetch data to display
   	// this.loading is updated to false on success
   	this.getUserInfo();
 
-    // Update last transaction every 15 seconds
-    this.otherDataInterval = window.setInterval(() => {
-      this.lastTransactionID = this._arweave.getLastTransactionID(this.mainAddress);
-      this.balance = this._arweave.getAccountBalance(this.mainAddress);
-      // this.getUserInfo();
-    }, 30000);
+    // init community
+    this._wisdomWizardsToken.initCommunity( 
+      this._arweave.arweave,
+      this._auth.getPrivateKey()
+    );
+
+    // get pst balance
+    this._wisdomWizardsToken.getPSTBalance(this.mainAddress).subscribe({
+      next: (balance: any) => {
+        this.balancePST = balance;
+      },
+      error: (error) => {
+        this.message(`Error: ${error}`, error);
+      }
+    });
+    
 
   }
 
