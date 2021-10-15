c63376e0223b1ea665c464b8f89a473917da1bfd
technodromex <richie.isc@gmail.com>
2021-10-14 20:43:00 -0500

    Update home animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { ArweaveService } from '../core/arweave.service';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 import anime from 'animejs';
-
+declare const document: any;
 
 @Component({
   selector: 'app-home',
@@ -38,20 +38,74 @@ export class HomeComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.animateWisdomTxt();
+    // Wrap every letter in a span
+    this.createWrapper('.txt-color-anime');
+    this.createWrapper('.secondary-title');
+    this.animateWisdomTxt('.txt-color-anime');
+    this.animateSubWisdomTxt('.secondary-title', false, false);
   }
 
-  animateWisdomTxt() {
-    anime({
-      targets: '.txt-color-anime',
-      color: this.randomColor,
-      duration: 3000,
-      direction: 'alternate',
-      easing: 'linear',
-      complete: () => {
-        this.animateWisdomTxt();
-      }
-    });
+  createWrapper(_container: string) {
+    var textWrapper = document.querySelector(_container);
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  }
+
+  animateWisdomTxt(
+    _txtContainer: string,
+    _animateColors: boolean = true,
+    _loop: boolean = true) {
+    anime.timeline({loop: _loop})
+      .add({
+        targets: _txtContainer + ' .letter',
+        opacity: [0,1],
+        easing: "easeInOutQuad",
+        duration: 2250,
+        delay: (el, i) => 150 * (i+1)
+      }).add({
+        targets: _txtContainer + ' .letter',
+        color: () => {
+          let color = '';
+          if (_animateColors) {
+            color = this.randomColor();
+          }
+          return color;
+        },
+        duration: 8000,
+        direction: 'alternate',
+        easing: 'linear',
+      }).add({
+        targets: _txtContainer,
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+      });
+  }
+
+  animateSubWisdomTxt(
+    _txtContainer: string,
+    _animateColors: boolean = true,
+    _loop: boolean = true) {
+    anime.timeline({loop: _loop})
+      .add({
+        targets: _txtContainer + ' .letter',
+        opacity: [0,1],
+        easing: "easeInOutQuad",
+        duration: 2250,
+        delay: (el, i) => 150 * (i+1)
+      }).add({
+        targets: _txtContainer + ' .letter',
+        color: () => {
+          let color = '';
+          if (_animateColors) {
+            color = this.randomColor();
+          }
+          return color;
+        },
+        duration: 8000,
+        direction: 'alternate',
+        easing: 'linear',
+      });
   }
 
   randomColor() {
